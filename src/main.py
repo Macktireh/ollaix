e37aa433606bd6934046ab8a1083b34d30781d74
Macktireh <abdimack97@gmail.com>
2025-07-11 18:50:29 +0200

    feat: initialize main application and routes setup

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from litestar import Litestar
+from litestar.config.cors import CORSConfig
+from litestar.exceptions import HTTPException, ImproperlyConfiguredException, ValidationException
+
+from config.exception_handler import app_exception_handler
+from config.settings import CORS_ALLOWED_ORIGINS, DEBUG, openapi_config
+from routes import routes
+
+cors_config = CORSConfig(allow_origins=CORS_ALLOWED_ORIGINS)
+
+
+app = Litestar(
+    route_handlers=routes,
+    openapi_config=openapi_config,
+    debug=DEBUG,
+    cors_config=cors_config,
+    exception_handlers={
+        HTTPException: app_exception_handler,
+        ImproperlyConfiguredException: app_exception_handler,
+        ValidationException: app_exception_handler,
+    },
+)
